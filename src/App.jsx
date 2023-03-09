750da3482e5af6494dbd709d58b6d5d1dd181fc7
kevin villarruel <kevinevolutivos@gmail.com>
2023-03-09 12:46:55 -0500

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 
+
 const App = () => {
   const [users, setUsers] = useState([]);
   const [temperature, setmoveTemperature] = useState(false);
@@ -12,7 +13,7 @@ const App = () => {
       longitud = position.coords.longitude;
       latitud = position.coords.latitude;
       const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitud}&lon=${longitud}&appid=c2f093589d8a1edff669b918f14909c0`;
-      console.log(url)
+
       fetch(url)
         .then((response) => {
           return response.json();
@@ -31,7 +32,15 @@ const App = () => {
   };
 
   return (
-    <div className="h-full flex flex-col justify-center items-center p-10">      
+    <div className=" flex flex-col justify-center items-center p-10">
+      <div className="">
+        <img
+          className=""
+          src={`https://openweathermap.org/img/wn/${users.weather?.[0].icon}@4x.png`}
+          alt=""
+        />
+      </div>
+
       <p className="pl-2 text-5xl pb-6 pt-10">
         {temperature
           ? Math.round(fahrenheit(users.main?.temp - 273.15)) + '°F'
@@ -42,8 +51,9 @@ const App = () => {
         <p>CLOUDS: {users.clouds?.all} %</p>
         <p>PRESSURE: {users.main?.pressure} hPa</p>
       </div>
+
       <div className="relative">
-        <p className="pt-6 pl-6 text-xl font-semibold">
+        <p className="text-xl font-semibold">
           {users.name}, {users.sys?.country}
         </p>
         <p className="absolute left-60 bottom-1 text-right">
