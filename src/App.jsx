29f0d0c0b1dcdabb5759a8fc88b55ed6b8c6723b
kevin villarruel <kevinevolutivos@gmail.com>
2023-04-08 20:22:15 -0500

    finish2

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 
-
 const App = () => {
   const [users, setUsers] = useState([]);
   const [temperature, setmoveTemperature] = useState(false);
@@ -40,33 +39,33 @@ const App = () => {
           alt=""
         />
       </div>
-
-      <p className="pl-2 text-5xl pb-6 pt-10">
-        {temperature
-          ? Math.round(fahrenheit(users.main?.temp - 273.15)) + '°F'
-          : Math.round(users.main?.temp - 273.15) + '°C'}
-      </p>
-      <div className="pl-3 text-xs flex flex-col gap-2">
-        <p>WIND: {users.wind?.speed} m/s</p>
-        <p>CLOUDS: {users.clouds?.all} %</p>
-        <p>PRESSURE: {users.main?.pressure} hPa</p>
-      </div>
-
-      <div className="relative">
-        <p className="text-xl font-semibold">
-          {users.name}, {users.sys?.country}
+      <button
+        className="bg-teal-600 rounded-xl text-white px-8 py-2 font-semibold mb-9"
+        onClick={moveTemperature}
+      >
+        {temperature ? 'Toggle to C°' : 'Toggle to F°'}
+      </button>
+      <div className="bg-teal-600 w-60  flex  flex-col items-center rounded-t-lg rounded-b-lg">
+        <p className="pl-2 text-5xl pb-6 ">
+          {temperature
+            ? Math.round(fahrenheit(users.main?.temp - 273.15)) + '°F'
+            : Math.round(users.main?.temp - 273.15) + '°C'}
         </p>
-        <p className="absolute left-60 bottom-1 text-right">
-          {users.weather?.[0].description}
-        </p>
-      </div>
-      <div className="flex justify-center mt-11">
-        <button
-          className="bg-purple-700 rounded-xl text-white px-8 py-2 font-semibold"
-          onClick={moveTemperature}
-        >
-          {temperature ? 'Toggle to C°' : 'Toggle to F°'}
-        </button>
+        <div className="pl-3 text-xs flex flex-col gap-2">
+          <p>WIND: {users.wind?.speed} m/s</p>
+          <p>CLOUDS: {users.clouds?.all} %</p>
+          <p>PRESSURE: {users.main?.pressure} hPa</p>
+        </div>
+
+        <div className="relative">
+          <p className="text-xl font-semibold">
+            {users.name}, {users.sys?.country}
+          </p>
+          <p className="bg-teal-600 absolute bottom-1 text-right rounded-t-lg rounded-b-lg p-2 left-44 h-10 w-20">
+            {users.weather?.[0].description}
+          </p>
+        </div>
+        <div className="flex justify-center mt-11"></div>
       </div>
     </div>
   );
